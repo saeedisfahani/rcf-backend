2eb00ce783dee444d3121356a1c40f337d60b1bb
saeed <saeed.com2000@gmail.com>
2021-11-19 06:55:12 +0330

    Create Models

---
+++ src/database/migrations/2021_11_19_023159_create_answers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->string('content');
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onDelete('CASCADE');
+            $table->foreignId('thread_id')
+                ->constrained()
+                ->onDelete('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
